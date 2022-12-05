a5f15255e86481820b60bb66091aee48eb674756
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2022-12-05 22:54:11 +0200

    scaffold/common widgets

--- lib/Common_widgets/scaffoldd.dart
+++ lib/Common_widgets/scaffoldd.dart
@@ -3,15 +3,20 @@ import 'package:flutter/material.dart';
 class Scaffoldd extends StatelessWidget {
   final bcolor;
   final body;
+  final bNBar;
 
   const Scaffoldd(
-      {super.key, this.bcolor = const Color(0xFF007CD7), required this.body});
+      {super.key,
+      this.bcolor = const Color(0xFF007CD7),
+      required this.body,
+      this.bNBar});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bcolor,
       body: body,
+      bottomNavigationBar: bNBar,
     );
   }
 }
