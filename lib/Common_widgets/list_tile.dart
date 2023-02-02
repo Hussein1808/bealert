72cd5a97ed09b840a4c2b6b1397da4ebeae8c745
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-02 23:02:47 +0200

    bottom settings data

---
+++ lib/Common_widgets/list_tile.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:bealert/Common_widgets/scaffoldd.dart';
+import 'package:bealert/Common_widgets/sizedboxx.dart';
+import 'package:bealert/Common_widgets/textt.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:bealert/Common_widgets/containerr.dart';
+import 'package:unicons/unicons.dart';
+
+class ListTilee extends StatefulWidget {
+  final title;
+  final icon;
+
+  const ListTilee({
+    super.key,
+    required this.title,
+    this.icon,
+  });
+
+  @override
+  State<ListTilee> createState() => _ListTileeState();
+}
+
+class _ListTileeState extends State<ListTilee> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Containerr(
+      decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          borderRadius: BorderRadius.circular(25)),
+      child: ListTile(
+        minLeadingWidth: 10,
+        leading: Icon(widget.icon),
+        title: Textt(
+            text: widget.title, color: Theme.of(context).colorScheme.primary),
+      ),
+    );
+  }
+}
---
+++ lib/Settings/Data/const.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/widgets.dart';
+import 'package:unicons/unicons.dart';
+
+List<String> tiles = [
+  'Edit Profile',
+  'Notifications',
+  'Clear History',
+  'About',
+  'Logout'
+];
+List<IconData> icons = [
+  UniconsLine.user,
+  UniconsLine.bell,
+  UniconsLine.trash,
+  UniconsLine.info_circle,
+  UniconsLine.sign_out_alt
+];
