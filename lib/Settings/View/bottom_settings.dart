5179af84140d31aa038968de502a9f09d39ef859
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-02 23:02:22 +0200

    bottom settings screen

---
+++ lib/Settings/View/bottom_settings.dart
@@ -0,0 +1,43 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, unused_local_variable,prefer_const_constructors
+import 'package:bealert/Common_widgets/containerr.dart';
+import 'package:bealert/Common_widgets/list_tile.dart';
+import 'package:bealert/Common_widgets/sizedboxx.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:quiver/time.dart';
+import 'package:swipe/swipe.dart';
+import 'package:unicons/unicons.dart';
+import '../../Common_widgets/textt.dart';
+import '../Data/const.dart';
+
+class BottomSettings extends StatefulWidget {
+  const BottomSettings({super.key});
+
+  @override
+  State<BottomSettings> createState() => _BottomSettingsState();
+}
+
+class _BottomSettingsState extends State<BottomSettings> {
+  @override
+  Widget build(BuildContext context) {
+    //* Screen size
+    final screenwidth = MediaQuery.of(context).size.width;
+    final screenheight = MediaQuery.of(context).size.height;
+    return Container(
+        child: ListView.builder(
+      itemCount: tiles.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: ListTilee(
+            title: tiles[index],
+            icon: icons[index],
+          ),
+        );
+      },
+    ));
+  }
+}
