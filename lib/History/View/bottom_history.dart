f3ac025e6db9921ef11591c6dae6f444c8d77993
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-04 17:39:09 +0200

    bottom history

---
+++ lib/History/View/bottom_history.dart
@@ -0,0 +1,33 @@
+import 'package:bealert/Common_widgets/containerr.dart';
+import 'package:bealert/History/Data/tiles_data.dart';
+//ignore_for_file:prefer_const_constructors
+import 'package:flutter/material.dart';
+import '../Widgets/list_tile_history.dart';
+
+class BottomHistory extends StatefulWidget {
+  const BottomHistory({super.key});
+
+  @override
+  State<BottomHistory> createState() => _BottomHistoryState();
+}
+
+class _BottomHistoryState extends State<BottomHistory> {
+  @override
+  Widget build(BuildContext context) {
+    //* Screen size
+    final screenwidth = MediaQuery.of(context).size.width;
+    final screenheight = MediaQuery.of(context).size.height;
+    return ListView.builder(
+      itemCount: histories.length,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: HistoryListTilee(
+            title: histories[index].date,
+            trailing: histories[index].drowsydays,
+          ),
+        );
+      },
+    );
+  }
+}
