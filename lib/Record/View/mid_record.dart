878530f716ecedd1806d3342d9a04f6c15b82100
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-12 15:41:40 +0200

    mid record

---
+++ lib/Record/View/mid_record.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MidRecord extends StatefulWidget {
+  const MidRecord({super.key});
+
+  @override
+  State<MidRecord> createState() => _MidRecordState();
+}
+
+class _MidRecordState extends State<MidRecord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
