05e2f2b6d5a0fd5ab3593c0660267542d47cd7d9
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-12 15:41:47 +0200

    top record

---
+++ lib/Record/View/top_record.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TopRecord extends StatefulWidget {
+  const TopRecord({super.key});
+
+  @override
+  State<TopRecord> createState() => _TopRecordState();
+}
+
+class _TopRecordState extends State<TopRecord> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
