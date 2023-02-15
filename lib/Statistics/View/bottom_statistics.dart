1a96ff9f7a845a5ac5e98e13d30d53d0efb0a9d0
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-15 16:01:03 +0200

    bottom stat

--- lib/Statistics/View/bottom_statistics.dart
+++ lib/Statistics/View/bottom_statistics.dart
@@ -2,7 +2,9 @@ import 'package:bealert/Common_widgets/containerr.dart';
 import 'package:bealert/History/Data/tiles_data.dart';
 //ignore_for_file:prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+import '../../Common_widgets/textt.dart';
 import '../../History/Widgets/list_tile_history.dart';
 
 class BottomStatistics extends StatefulWidget {
@@ -18,17 +20,34 @@ class _BottomStatisticsState extends State<BottomStatistics> {
     //* Screen size
     final screenwidth = MediaQuery.of(context).size.width;
     final screenheight = MediaQuery.of(context).size.height;
-    return ListView.builder(
-      itemCount: histories.length,
-      itemBuilder: (BuildContext context, int index) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: HistoryListTilee(
-            title: histories[index].date,
-            trailing: histories[index].drowsydays,
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Textt(
+            text: 'Events',
+            size: 35.0,
+            weight: FontWeight.w500,
+            font: GoogleFonts.righteous,
           ),
-        );
-      },
+        ),
+        Container(
+          height: screenheight,
+          width: screenwidth,
+          child: ListView.builder(
+            itemCount: histories.length,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: HistoryListTilee(
+                  title: histories[index].date,
+                  trailing: histories[index].drowsydays,
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
