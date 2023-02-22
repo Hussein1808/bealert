eeb172d5faaa0a09c7f2a249dc5b925b01b00150
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-22 17:32:14 +0200

    modal domain + pubspec

--- lib/Settings/Domain/modals_domain.dart
+++ lib/Settings/Domain/modals_domain.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 class Modal {
   String title;
+  String? mtitle;
+  String? mtext;
   IconData icon;
   String? redirection;
 
-  Modal({required this.title, required this.icon, this.redirection});
+  Modal({
+    required this.title,
+    required this.icon,
+    this.redirection,
+    this.mtitle,
+    this.mtext,
+  });
 }
