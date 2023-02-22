bf1f45d8d575111c1346323c1b3dd44113850ec0
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-22 17:32:22 +0200

    modals data

--- lib/Settings/Data/modals_data.dart
+++ lib/Settings/Data/modals_data.dart
@@ -7,5 +7,15 @@ final List<Modal> modals = [
     title: 'Clear History',
     icon: UniconsLine.trash,
     redirection: '/clearhistory_page',
+    mtitle:'Clear History',
+    mtext:'Are you sure you want to clear history?'
+
+  ),
+  Modal(
+    title: 'logout',
+    icon: UniconsLine.sign_out_alt,
+    redirection: '/clearhistory_page',
+    mtitle:'Logout',
+    mtext:'Are you sure you want to logout?',
   ),
 ];
