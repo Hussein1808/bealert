cc8acc9d7c8d93cd48234ccc0e934106887c6b6c
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-24 19:11:51 +0200

    your info page - in progress

--- lib/User/View/your_info_page.dart
+++ lib/User/View/your_info_page.dart
@@ -7,9 +7,11 @@ import 'package:bealert/Common_widgets/scaffoldd.dart';
 import 'package:bealert/Common_widgets/sizedboxx.dart';
 import 'package:bealert/Common_widgets/textt.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:bealert/Common_widgets/containerr.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:unicons/unicons.dart';
 
 class Your_Info_Page extends StatefulWidget {
@@ -20,95 +22,283 @@ class Your_Info_Page extends StatefulWidget {
 }
 
 class _Your_Info_PageState extends State<Your_Info_Page> {
+  bool _isVisible = false;
+  bool _isVisible2 = false;
+  final RegExp namevalid = RegExp(r'(^[a-zA-Z]+$)');
+  final RegExp emailvalid = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+  final RegExp passwordvalid =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+  final formKey = GlobalKey<FormState>();
+  final formKey2 = GlobalKey<FormState>();
+  final formKey3 = GlobalKey<FormState>();
+  final formKey4 = GlobalKey<FormState>();
+  final formKey5 = GlobalKey<FormState>();
+  final formKey6 = GlobalKey<FormState>();
+
+  final TextEditingController pw = TextEditingController();
   @override
   Widget build(BuildContext context) {
     final screenwidth = MediaQuery.of(context).size.width;
     final screenheight = MediaQuery.of(context).size.height;
     return Scaffoldd(
-      bcolor: Theme.of(context).colorScheme.secondary,
-
+        bcolor: Theme.of(context).colorScheme.secondary,
         body: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Containerr(
-          w: screenwidth,
-          h: screenheight * 0.85,
-          decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50.0),
-                topRight: Radius.circular(50.0)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(35.0, 30.0, 35.0, 5.0),
-            child: Center(
-              child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
-                child: Column(
-                  children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                      IconButton(
-                        onPressed: (() =>
-                            GoRouter.of(context).go('/signup_page')),
-                        icon: Icon(
-                          UniconsLine.angle_left,
-                        ),
-                        iconSize: 50,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(child: SizedBox.shrink()),
+            Expanded(
+              flex: 8,
+              child: Containerr(
+                w: screenwidth,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50.0),
+                      topRight: Radius.circular(50.0)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(35.0, 30.0, 35.0, 5.0),
+                  child: Center(
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                IconButton(
+                                  onPressed: (() =>
+                                      GoRouter.of(context).go('/signup_page')),
+                                  icon: Icon(
+                                    UniconsLine.angle_left,
+                                  ),
+                                  iconSize: 50,
+                                ),
+                                Textt(
+                                  text: 'Your Info',
+                                  size: 48.0,
+                                  font: GoogleFonts.righteous,
+                                ),
+                              ]),
+                          //* full name field
+                          Form(
+                            key: formKey,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Required';
+                                } else if (!namevalid.hasMatch(value) &&
+                                    !RegExp(r"\s").hasMatch(value)) {
+                                  return 'Name must be alphabets';
+                                } else if (value.length < 3) {
+                                  return 'Name must be atleast 3 characters';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Full Name",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.user,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          //* Address field
+                          Form(
+                            key: formKey2,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value2) {
+                                if (value2!.isEmpty) {
+                                  return 'Required';
+                                } else if (!emailvalid.hasMatch(value2)) {
+                                  return 'Invalid email format';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Address",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.location_point,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          //* National ID field
+                          Form(
+                            key: formKey3,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              controller: pw,
+                              obscureText: !_isVisible,
+                              validator: (value3) {
+                                if (value3!.isEmpty) {
+                                  return 'Required';
+                                } else if (!passwordvalid.hasMatch(value3)) {
+                                  return 'Password must contain atleast 1 uppercase, 1 lowercase, 1 number and 1 special character';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "National ID",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.credit_card,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          //* Mobile number
+                          Form(
+                            key: formKey4,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: InternationalPhoneNumberInput(
+                              onInputChanged: (PhoneNumber number) {},
+                              onInputValidated: (bool value) {},
+                              selectorConfig: SelectorConfig(
+                                selectorType:
+                                    PhoneInputSelectorType.BOTTOM_SHEET,
+                              ),
+                              selectorTextStyle: TextStyle(color: Colors.black),
+                              ignoreBlank: false,
+                              formatInput: true,
+                              keyboardType: TextInputType.numberWithOptions(
+                                  signed: true, decimal: true),
+                              inputBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          //* Emergency contact
+                          Form(
+                            key: formKey5,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: InternationalPhoneNumberInput(
+                              onInputChanged: (PhoneNumber number) {},
+                              onInputValidated: (bool value) {},
+                              selectorConfig: SelectorConfig(
+                                selectorType:
+                                    PhoneInputSelectorType.BOTTOM_SHEET,
+                              ),
+                              hintText: 'Emergency Contact',
+                              selectorTextStyle: TextStyle(color: Colors.black),
+                              ignoreBlank: false,
+                              formatInput: true,
+                              keyboardType: TextInputType.numberWithOptions(
+                                  signed: true, decimal: true),
+                              inputBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          //* Blood group
+                          Form(
+                            key: formKey6,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              obscureText: !_isVisible2,
+                              validator: (value6) {
+                                if (value6 != pw.text) {
+                                  return 'Password does not match';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Blood Group",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.medical_drip,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(h: 30.0),
+                          //* Sign up button
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              SizedBox(
+                                height: 80.0,
+                                child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      backgroundColor:
+                                          Theme.of(context).splashColor,
+                                      shape: CircleBorder(),
+                                    ),
+                                    onPressed: () {
+                                      // if (formKey.currentState!.validate() &&
+                                      //     formKey2.currentState!.validate() &&
+                                      //     formKey3.currentState!.validate() &&
+                                      //     formKey4.currentState!.validate()) {
+                                      GoRouter.of(context)
+                                          .go('/your_info_page');
+                                      // }
+                                    },
+                                    child: Icon(
+                                      UniconsLine.arrow_right,
+                                      size: 42.0,
+                                    )),
+                              )
+                            ],
+                          ),
+                        ],
                       ),
-                      Textt(
-                        text: 'Your Info',
-                        size: 48.0,
-                        font: GoogleFonts.righteous,
-                      ),
-                    ]),
-                    SizedBoxx(h: 40.0),
-                    FormFieldd(hint: 'Full name', icon: UniconsLine.user),
-                    SizedBoxx(),
-                    FormFieldd(hint: 'Address', icon: UniconsLine.envelope),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'National ID',
-                      icon: UniconsLine.credit_card,
                     ),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Mobile number',
-                      icon: UniconsLine.phone,
-                    ),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Emergency contact',
-                      icon: UniconsLine.heart_rate,
-                    ),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Blood group',
-                      icon: UniconsLine.medical_drip,
-                    ),
-                    SizedBoxx(h: 32.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        FormButtons(
-                          bcolor: Theme.of(context).splashColor,
-                          tcolor: Theme.of(context).primaryColor,
-                          text: 'Next',
-                          redirect: '/vehicle_info_page',
-                          border: Border.all(
-                              width: 6.0, color: Theme.of(context).splashColor),
-                          icon: UniconsLine.angle_right_b,
-                          icolor: Theme.of(context).scaffoldBackgroundColor,
-                        ),
-                      ],
-                    ),
-                    SizedBoxx(h: 146.0),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-      ],
-    ));
+          ],
+        ));
   }
 }
