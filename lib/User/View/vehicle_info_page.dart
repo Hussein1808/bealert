b4af9693a5b7365a56beb32f2d2fa464565be00f
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-02-26 19:06:59 +0200

    vechile info page

--- lib/User/View/vehicle_info_page.dart
+++ lib/User/View/vehicle_info_page.dart
@@ -20,97 +20,275 @@ class Vehicle_Info_Page extends StatefulWidget {
 }
 
 class _Vehicle_Info_PageState extends State<Vehicle_Info_Page> {
+  static GlobalKey<FormState> formKey7 = new GlobalKey<FormState>();
+  static GlobalKey<FormState> formKey8 = new GlobalKey<FormState>();
+  static GlobalKey<FormState> formKey9 = new GlobalKey<FormState>();
+  static GlobalKey<FormState> formKey10 = new GlobalKey<FormState>();
+  static GlobalKey<FormState> formKey11 = new GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     final screenwidth = MediaQuery.of(context).size.width;
     final screenheight = MediaQuery.of(context).size.height;
-    return Scaffoldd(
-      bcolor: Theme.of(context).colorScheme.secondary,
 
+    final RegExp namevalid = RegExp(r'(^[a-zA-Z]+$)');
+
+    final RegExp number = RegExp(r'^\d{1,4}$');
+
+    return Scaffoldd(
+        bcolor: Theme.of(context).colorScheme.secondary,
         body: Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        Containerr(
-          w: screenwidth,
-          h: screenheight * 0.85,
-          decoration: BoxDecoration(
-            color: Theme.of(context).scaffoldBackgroundColor,
-            borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(50.0),
-                topRight: Radius.circular(50.0)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(30.0, 30.0, 35.0, 5.0),
-            child: Center(
-              child: SingleChildScrollView(
-                physics: BouncingScrollPhysics(),
-                child: Column(
-                  children: [
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                      IconButton(
-                        onPressed: (() =>
-                            GoRouter.of(context).go('/your_info_page')),
-                        icon: Icon(
-                          UniconsLine.angle_left,
-                        ),
-                        iconSize: 50,
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Expanded(child: SizedBox.shrink()),
+            Expanded(
+              flex: 8,
+              child: Containerr(
+                w: screenwidth,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).scaffoldBackgroundColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50.0),
+                      topRight: Radius.circular(50.0)),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(30.0, 30.0, 35.0, 5.0),
+                  child: Center(
+                    child: SingleChildScrollView(
+                      physics: BouncingScrollPhysics(),
+                      child: Column(
+                        children: [
+                          Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                IconButton(
+                                  onPressed: (() => GoRouter.of(context)
+                                      .go('/your_info_page')),
+                                  icon: Icon(
+                                    UniconsLine.angle_left,
+                                  ),
+                                  iconSize: 50,
+                                ),
+                                Textt(
+                                  text: 'Vehicle Info',
+                                  size: 48.0,
+                                  font: GoogleFonts.righteous,
+                                ),
+                              ]),
+                          SizedBoxx(h: 30.0),
+                          Form(
+                            key: formKey7,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Required';
+                                } else if (!namevalid.hasMatch(value) &&
+                                    !RegExp(r"\s").hasMatch(value)) {
+                                  return 'Name must be alphabets';
+                                } else if (value.length < 3) {
+                                  return 'Name must be atleast 3 characters';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Owner's name",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.user,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          Form(
+                            key: formKey8,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value2) {
+                                if (value2!.isEmpty) {
+                                  return 'Required';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Brand",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.car,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          Form(
+                            key: formKey9,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value3) {
+                                if (value3!.isEmpty) {
+                                  return 'Required';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Color",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.palette,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          Row(
+                            children: [
+                              Textt(
+                                text: 'License plate',
+                                size: 20.0,
+                                weight: FontWeight.bold,
+                              ),
+                            ],
+                          ),
+                          SizedBoxx(),
+                          Form(
+                            key: formKey10,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              keyboardType: TextInputType.number,
+                              validator: (value4) {
+                                if (value4!.isEmpty) {
+                                  return 'Required';
+                                } else if (!number.hasMatch(value4)) {
+                                  return 'The number must have up to 4 digits and contain only numeric digits';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Numbers",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  Icons.numbers,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(),
+                          Form(
+                            key: formKey11,
+                            autovalidateMode:
+                                AutovalidateMode.onUserInteraction,
+                            child: TextFormField(
+                              validator: (value5) {
+                                if (value5!.isEmpty) {
+                                  return 'Required';
+                                } else {
+                                  return null;
+                                }
+                              },
+                              decoration: InputDecoration(
+                                errorMaxLines: 2,
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                hintText: "Letters",
+                                hintStyle: TextStyle(
+                                  fontSize: 18.0,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                                prefixIcon: Icon(
+                                  UniconsLine.letter_english_a,
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBoxx(h: 32.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Expanded(
+                                child: SizedBox(
+                                  height: screenheight * 0.07,
+                                  child: ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor:
+                                            Theme.of(context).splashColor,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30.0),
+                                        ),
+                                      ),
+                                      onPressed: () {
+                                        if (formKey7.currentState!.validate() &&
+                                            formKey8.currentState!.validate() &&
+                                            formKey9.currentState!.validate() &&
+                                            formKey10.currentState!
+                                                .validate() &&
+                                            formKey11.currentState!
+                                                .validate()) {
+                                          GoRouter.of(context)
+                                              .go('/Login_page');
+                                        }
+                                      },
+                                      child: Textt(
+                                          text: 'Finish',
+                                          size: 24.0,
+                                          color: Theme.of(context).primaryColor,
+                                          weight: FontWeight.bold)),
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBoxx(h: 46.0),
+                        ],
                       ),
-                      Textt(
-                        text: 'Vehicle Info',
-                        size: 48.0,
-                        font: GoogleFonts.righteous,
-                      ),
-                    ]),
-                    SizedBoxx(h: 40.0),
-                    FormFieldd(hint: 'Owner', icon: UniconsLine.user),
-                    SizedBoxx(),
-                    FormFieldd(hint: 'Brand', icon: UniconsLine.car),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Color',
-                      icon: UniconsLine.palette,
                     ),
-                    SizedBoxx(),
-                    Row(
-                      children: [
-                        Textt(
-                          text: 'License plate',
-                          size: 20.0,
-                        ),
-                      ],
-                    ),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Numbers',
-                    ),
-                    SizedBoxx(),
-                    FormFieldd(
-                      hint: 'Letters',
-                    ),
-                    SizedBoxx(h: 32.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        FormButtons(
-                          bcolor: Theme.of(context).splashColor,
-                          tcolor: Theme.of(context).primaryColor,
-                          text: 'Next',
-                          redirect: '/login_page',
-                          border: Border.all(
-                              width: 6.0, color: Theme.of(context).splashColor),
-                          icon: UniconsLine.angle_right_b,
-                          icolor: Theme.of(context).scaffoldBackgroundColor,
-                        ),
-                      ],
-                    ),
-                    SizedBoxx(h: 46.0),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ),
-      ],
-    ));
+          ],
+        ));
   }
 }
