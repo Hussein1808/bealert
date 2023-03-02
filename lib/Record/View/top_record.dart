f8467321a375b04de71404c36745772a7e0d2d3c
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-03-02 16:15:13 +0200

    top record phase 1 done

--- lib/Record/View/top_record.dart
+++ lib/Record/View/top_record.dart
@@ -21,102 +21,100 @@ class _TopRecordState extends State<TopRecord> {
   Widget build(BuildContext context) {
     final screenwidth = MediaQuery.of(context).size.width;
     final screenheight = MediaQuery.of(context).size.height;
-    return Column(
-      children: [
-        Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                width: screenwidth * 0.15,
-                child: FloatingActionButton(
-                  heroTag: "btn1",
-                  elevation: 0.0,
-                  backgroundColor: Theme.of(context).colorScheme.secondary,
-                  onPressed: () {
-                    showMaterialModalBottomSheet(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                      ),
-                      backgroundColor:
-                          Theme.of(context).scaffoldBackgroundColor,
-                      context: context,
-                      builder: (context) => SingleChildScrollView(
-                        controller: ModalScrollController.of(context),
-                        child: Container(
-                          height: screenheight * 0.85,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Expanded(child: SizedBox.shrink()),
-                              Expanded(child: Textt(text: 'Time')),
-                              Expanded(
-                                flex: 2,
-                                child: Textt(
-                                  text: '00:44:24',
-                                  size: 72.0,
-                                  font: GoogleFonts.firaSans,
-                                ),
-                              ),
-                              Div(),
-                              Expanded(child: Textt(text: 'Drowsiness Level')),
-                              Expanded(
-                                flex: 3,
-                                child: CircularSeekBar(
-                                  width: double.infinity,
-                                  height: 250,
-                                  progress: 100,
-                                  barWidth: 9,
-                                  startAngle: 90,
-                                  sweepAngle: 180,
-                                  strokeCap: StrokeCap.round,
-                                  progressGradientColors: [
-                                    Color.fromARGB(255, 68, 243, 33),
-                                    Color.fromARGB(255, 255, 234, 0),
-                                    Color.fromARGB(255, 255, 183, 0),
-                                    Color.fromARGB(255, 255, 0, 0)
-                                  ],
-                                  dashWidth: 37,
-                                  dashGap: 10,
-                                  animation: true,
-                                  child: Center(
-                                    child: Textt(
-                                      text: '4',
-                                      size: 56.0,
-                                      font: GoogleFonts.firaSans,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Div(),
-                              Expanded(child: Textt(text: 'Distance')),
-                              Expanded(
-                                flex: 2,
-                                child: Textt(
-                                  text: '0.02',
-                                  size: 72.0,
-                                  font: GoogleFonts.firaSans,
-                                ),
-                              ),
-                              Expanded(child: Textt(text: 'Kilometers')),
-                            ],
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0.0,
+        leading: IconButton(
+          icon: Icon(UniconsLine.arrow_left),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: Center(child: Textt(text: 'Record', weight: FontWeight.bold)),
+        actions: [
+          IconButton(
+            icon: Icon(
+              UniconsLine.graph_bar,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+            onPressed: () {
+              showMaterialModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                context: context,
+                builder: (context) => SingleChildScrollView(
+                  controller: ModalScrollController.of(context),
+                  child: Container(
+                    height: screenheight * 0.85,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(child: SizedBox.shrink()),
+                        Expanded(child: Textt(text: 'Time')),
+                        Expanded(
+                          flex: 2,
+                          child: Textt(
+                            text: '00:44:24',
+                            size: 72.0,
+                            font: GoogleFonts.firaSans,
                           ),
                         ),
-                      ),
-                    );
-                  },
-                  child: Icon(UniconsLine.graph_bar,
-                      color: Theme.of(context).scaffoldBackgroundColor,
-                      size: 30),
+                        Div(),
+                        Expanded(child: Textt(text: 'Drowsiness Level')),
+                        Expanded(
+                          flex: 3,
+                          child: CircularSeekBar(
+                            width: double.infinity,
+                            height: 250,
+                            progress: 100,
+                            barWidth: 9,
+                            startAngle: 90,
+                            sweepAngle: 180,
+                            strokeCap: StrokeCap.round,
+                            progressGradientColors: [
+                              Color.fromARGB(255, 68, 243, 33),
+                              Color.fromARGB(255, 255, 234, 0),
+                              Color.fromARGB(255, 255, 183, 0),
+                              Color.fromARGB(255, 255, 0, 0)
+                            ],
+                            dashWidth: 37,
+                            dashGap: 10,
+                            animation: true,
+                            child: Center(
+                              child: Textt(
+                                text: '4',
+                                size: 56.0,
+                                font: GoogleFonts.firaSans,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Div(),
+                        Expanded(child: Textt(text: 'Distance')),
+                        Expanded(
+                          flex: 2,
+                          child: Textt(
+                            text: '0.02',
+                            size: 72.0,
+                            font: GoogleFonts.firaSans,
+                          ),
+                        ),
+                        Expanded(child: Textt(text: 'Kilometers')),
+                      ],
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              );
+            },
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
