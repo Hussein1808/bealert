8f5f7c659e9013cce9f00f42622d253c34ba8e3f
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-03-03 18:19:32 +0200

    tiles data adding tripdistance

--- lib/History/Domain/tiles_domain.dart
+++ lib/History/Domain/tiles_domain.dart
@@ -1,6 +1,7 @@
 class History {
   String date;
   int drowsydays;
+  int tripdistance;
 
-  History({required this.date, required this.drowsydays});
+  History({required this.date, required this.drowsydays,required this.tripdistance});
 }
