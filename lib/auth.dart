176ac0a527f7c9cbdfcff2c439b2bc1089d5c91a
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-03-16 18:13:22 +0200

    auth to redirect

---
+++ lib/auth.dart
@@ -0,0 +1,27 @@
+import 'package:bealert/Common_widgets/scaffoldd.dart';
+import 'package:bealert/info.dart';
+import 'package:bealert/pages.dart';
+import 'package:bealert/start_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class Auth extends StatelessWidget {
+  const Auth({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return HomePage(
+                selectedIndex: 0,
+              );
+            } else {
+              return Start();
+            }
+          }),
+    );
+  }
+}
