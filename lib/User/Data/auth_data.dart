58d6c699542624f2fa866ded75d3cd48cf3e3f0b
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-04-16 15:40:12 +0200

    sign up auth update back end

--- lib/User/Data/auth_data.dart
+++ lib/User/Data/auth_data.dart
@@ -1,10 +1,48 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-Future<void> userSetup(String username, String fullname, String address, int nationalID, String phonenumber, String emergencycontact, String bloodgroup ) async {
+Future<void> userSetup(
+    String username,
+    String fullname,
+    String address,
+    int nationalID,
+    String phonenumber,
+    String emergencycontact,
+    String bloodgroup,
+    String owner,
+    String brand,
+    String color,
+    String letters,
+    int numbers) async {
   CollectionReference users = FirebaseFirestore.instance.collection('Users');
   FirebaseAuth auth = FirebaseAuth.instance;
   String uid = auth.currentUser!.uid.toString();
-  users.add({'Username': username, 'uid': uid, 'Fullname': fullname, 'Address': address, 'NationalID': nationalID, 'PhoneNumber': phonenumber, 'EmergencyContact': emergencycontact, 'BloodGroup': bloodgroup });
+  users.add({
+    'Username': username,
+    'uid': uid,
+    'Fullname': fullname,
+    'Address': address,
+    'NationalID': nationalID,
+    'PhoneNumber': phonenumber,
+    'EmergencyContact': emergencycontact,
+    'BloodGroup': bloodgroup,
+    'vehicle': {
+      'Owner': owner,
+      'uid': uid,
+      'Brand': brand,
+      'Color': color,
+      'Letters': letters,
+      'Numbers': numbers
+    }
+  });
+  // users.doc('vehicle').set({'Owner': owner, 'uid': uid, 'Brand': brand, 'Color': color, 'Letters': letters, 'Numbers': numbers });
+
   return;
 }
+// Future<void> vehicleUpdate(String owner, String brand, String color, String letters, int numbers ) async {
+//   CollectionReference users = FirebaseFirestore.instance.collection('Users');
+//   FirebaseAuth auth = FirebaseAuth.instance;
+//   String uid = auth.currentUser!.uid.toString();
+//   users.doc('vehicle').set({'Owner': owner, 'uid': uid, 'Brand': brand, 'Color': color, 'Letters': letters, 'Numbers': numbers });
+//   return;
+// }
