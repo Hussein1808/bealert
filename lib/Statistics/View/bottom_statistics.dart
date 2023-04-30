dd852bd3402db0997a503d95d3143e00f12ac798
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-05-01 01:32:52 +0200

    reading trips data at stat

--- lib/Statistics/View/bottom_statistics.dart
+++ lib/Statistics/View/bottom_statistics.dart
@@ -1,9 +1,10 @@
-
 import 'package:bealert/History/Data/tiles_data.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../../Common_widgets/textt.dart';
 import '../../History/Widgets/list_tile_history.dart';
+import '../../Record/Domain/trip_data_domain.dart';
+import '../../Record/Repository/trip_data_repo.dart';
 
 class BottomStatistics extends StatefulWidget {
   const BottomStatistics({super.key});
@@ -13,6 +14,20 @@ class BottomStatistics extends StatefulWidget {
 }
 
 class _BottomStatisticsState extends State<BottomStatistics> {
+  final TripsRepository _repository = TripsRepository();
+  List<Trips> _trips = [];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> _getTrips() async {
+    final trips = await _repository.getTrips();
+    setState(() {
+      _trips = trips;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     //* Screen size
@@ -21,7 +36,7 @@ class _BottomStatisticsState extends State<BottomStatistics> {
     return ListView(
       children: [
         const Padding(
-          padding:  EdgeInsets.all(4.0),
+          padding: EdgeInsets.all(4.0),
           child: Textt(
             text: 'Events',
             size: 35.0,
@@ -32,17 +47,35 @@ class _BottomStatisticsState extends State<BottomStatistics> {
         Container(
           height: screenheight,
           width: screenwidth,
-          child: ListView.builder(
-            itemCount: histories.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: HistoryListTilee(
-                  title: histories[index].date,
-                  trailing: histories[index].drowsydays,
-                  tripdistance: histories[index].tripdistance,
-                ),
-              );
+          child: FutureBuilder<List<Trips>>(
+            future: _repository.getTrips(),
+            
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                _trips = snapshot.data!;
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: HistoryListTilee(
+                        date: snapshot.data![index].date,
+                        distance: snapshot.data![index].distance,
+                        drowsinesstimes: snapshot.data![index].drowsinesstimes,
+                        time: snapshot.data![index].time,
+                      ),
+                    );
+                  },
+                );
+              } else if (snapshot.hasError) {
+                return  Center(
+                  child: Text('Error ${snapshot.error}'),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
             },
           ),
         ),
