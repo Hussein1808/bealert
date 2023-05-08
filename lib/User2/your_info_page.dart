f9629a88c79f9fb39601fb84bb40dfe0f156c108
Abdelrahman Elewa <abdelrahmanelewa6@gmail.com>
2023-05-08 22:07:47 +0300

    Update your_info_page.dart

--- lib/User2/your_info_page.dart
+++ lib/User2/your_info_page.dart
@@ -30,3 +30,29 @@ class Your_Info_Page extends StatefulWidget {
         password: password,
       );
 }
+
+class _Your_Info_PageState extends State<Your_Info_Page> {
+  String userName;
+  String email;
+  String password;
+  _Your_Info_PageState({
+    required this.userName,
+    required this.email,
+    required this.password,
+  });
+
+   bool _isVisible = false;
+  bool _isVisible2 = false;
+  final RegExp namevalid = RegExp(r'(^[a-zA-Z]+$)');
+  final RegExp emailvalid = RegExp(r'^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+');
+  final RegExp passwordvalid =
+      RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$');
+  final bloodGroupRegex = RegExp(r'^(A|B|AB|O)[+-]$');
+  final RegExp nationalIdRegex = RegExp(r'^[0-9]{1,}$');
+
+  final formKey = GlobalKey<FormState>();
+  final formKey2 = GlobalKey<FormState>();
+  final formKey3 = GlobalKey<FormState>();
+  final formKey4 = GlobalKey<FormState>();
+  final formKey5 = GlobalKey<FormState>();
+  final formKey6 = GlobalKey<FormState>();
