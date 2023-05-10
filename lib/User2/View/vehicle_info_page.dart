7913c13c0688c192f5a60abcec2143a183f673f1
Omar Mohamed Abdelrehim <omar1901044@miuegypt.edu.eg>
2023-05-10 15:56:13 +0300

    vehcile info widget

---
+++ lib/User2/View/vehicle_info_page.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
+
+import '../../Const/constants.dart';
+import 'package:bealert/User/Data/auth_data.dart';
+
+class Vehicle_Info_Page extends StatefulWidget {
+  String userName;
+  String email;
+  String password;
+  String fullName;
+ 
+  Vehicle_Info_Page(
+      {required this.userName,
+      required this.email,
+      required this.password,
+      required this.fullName,
+      super.key});
+
+  @override
+  State<Vehicle_Info_Page> createState() => _Vehicle_Info_PageState();
+}
+
+class _Vehicle_Info_PageState extends State<Vehicle_Info_Page> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
