fac81649e0b46bf493872d391a81dd6979a625d7
Hussein Hashad <hussein1907338@miuegypt.edu.eg>
2023-06-18 19:28:12 +0300

    about us

---
+++ lib/About us/View/about_us.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AboutUsPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: const Text(
+          'About Us',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 1.2,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      backgroundColor: Theme.of(context).colorScheme.secondary,
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 128, 8, 24),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to Be-Alert application!',
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'Dedicated to keeping you safe on the road',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white70,
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                'We are proud to introduce our cutting-edge technology that utilizes a brain sensor to detect your drowsiness level while driving, providing real-time alerts to ensure your safety.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white70,
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 24),
+              const Text(
+                'At Be-Alert, we understand the importance of staying alert and focused while behind the wheel. We have developed a seamless integration between Flutter and brain sensor technology to create a powerful tool that helps combat drowsiness-related accidents. Our application works effortlessly with your brain sensor to monitor your brainwave patterns and detect signs of drowsiness, alerting you promptly when your concentration levels begin to decrease.',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white70,
+                  letterSpacing: 1.2,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
